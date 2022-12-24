e206d8091eb1de0980a674bd1dfa6323d0e82195
yash91989201 <yashraj6370@gmail.com>
2022-12-24 19:49:44 +0530

    allow yes to apt install commands

--- script.sh
+++ script.sh
@@ -8,14 +8,14 @@ curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compo
 chmod +x ~/.docker/cli-plugins/docker-compose
 echo "3. Installing jenkins"
 apt update
-apt install openjdk-11-jre
+apt install openjdk-11-jre --yes
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 apt-get update
-apt-get install jenkins
+apt-get install jenkins --yes
 systemctl enable jenkins
 echo "4. Adding jenkins to docker group"
 systemctl stop jenkins
